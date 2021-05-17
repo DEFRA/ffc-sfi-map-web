7470cbae31aaa3de8e862a9ea29c5a92902d233e
John Watson <johnwatson484@hotmail.com>
2021-05-17 10:48:53 +0100

    Variables renamed

--- app/api/parcels.js
+++ app/api/parcels.js
@@ -2,7 +2,7 @@ const base = require('./base')
 const config = require('../config')
 
 const getParcels = async (sbi) => {
-  const url = `${config.publicApi}LandCovers/MapServer/0/query?where=SBI=${sbi}&outSR=27700&f=geojson`
+  const url = `${config.publicApi}LandCovers/MapServer/0/query?where=SBI=${sbi}&f=geojson`
   return base.get(url)
 }
 
