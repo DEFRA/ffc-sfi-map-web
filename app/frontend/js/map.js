4f385d02a23f9cdf5ec4cf7bee9b7f7dffea7e13
John Watson <johnwatson484@hotmail.com>
2021-05-17 12:45:19 +0100

    OL css added to webpack

--- app/frontend/js/map.js
+++ app/frontend/js/map.js
@@ -1,4 +1,4 @@
-// import 'ol/ol.css'
+import 'ol/ol.css'
 import GeoJSON from 'ol/format/GeoJSON'
 import Map from 'ol/Map'
 import View from 'ol/View'
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(?:s[ac]|c)ss$/i,
         use: [
           'style-loader',
           {
